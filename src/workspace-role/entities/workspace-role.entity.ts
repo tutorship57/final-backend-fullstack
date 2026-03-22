fe7eb8f5d526de332f9a2309594fcf9f833a5eec
Edward_Bacara <tutorship57@gmail.com>
2026-03-22 22:21:00 +0700

    feat:updated entity create & updated Date and updated Endpoint

--- src/workspace-role/entities/workspace-role.entity.ts
+++ src/workspace-role/entities/workspace-role.entity.ts
@@ -2,12 +2,14 @@ import { Permission } from 'src/permission/entities/permission.entity';
 import { Workspace } from 'src/workspace/entities/workspace.entity';
 import {
   Column,
+  CreateDateColumn,
   Entity,
   JoinColumn,
   JoinTable,
   ManyToMany,
   ManyToOne,
   PrimaryGeneratedColumn,
+  UpdateDateColumn,
 } from 'typeorm';
 
 @Entity('workspace_roles')
@@ -20,6 +22,12 @@ export class WorkspaceRole {
 
   @Column()
   workspace_id: string;
+
+  @CreateDateColumn({ name: 'created_at', type: 'timestamptz' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamptz' })
+  updatedAt: Date;
 
   @ManyToOne(() => Workspace)
   @JoinColumn({ name: 'workspace_id' })
--- src/workspace-role/workspace-role.service.ts
+++ src/workspace-role/workspace-role.service.ts
@@ -1,26 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateWorkspaceRoleDto } from './dto/create-workspace-role.dto';
 import { UpdateWorkspaceRoleDto } from './dto/update-workspace-role.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { WorkspaceRole } from './entities/workspace-role.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class WorkspaceRoleService {
+  constructor(
+    @InjectRepository(WorkspaceRole)
+    private readonly workspaceRoleRepo: Repository<WorkspaceRole>,
+  ) {}
   create(createWorkspaceRoleDto: CreateWorkspaceRoleDto) {
-    return 'This action adds a new workspaceRole';
+    const newWorkspaceRole = this.workspaceRoleRepo.create(
+      createWorkspaceRoleDto,
+    );
+
+    return this.workspaceRoleRepo.save(newWorkspaceRole);
   }
 
   findAll() {
-    return `This action returns all workspaceRole`;
+    return this.workspaceRoleRepo.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} workspaceRole`;
+  findOne(id: string) {
+    return this.workspaceRoleRepo.findOne({
+      where: {
+        id: id,
+      },
+    });
   }
 
-  update(id: number, updateWorkspaceRoleDto: UpdateWorkspaceRoleDto) {
-    return `This action updates a #${id} workspaceRole`;
+  async update(id: string, updateWorkspaceRoleDto: UpdateWorkspaceRoleDto) {
+    const existRole = await this.findOne(id);
+    if (!existRole) {
+      throw new NotFoundException();
+    }
+    const updated = this.workspaceRoleRepo.merge(
+      existRole,
+      updateWorkspaceRoleDto,
+    );
+    return this.workspaceRoleRepo.save(updated);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} workspaceRole`;
+  async remove(id: string) {
+    const removedUser = await this.workspaceRoleRepo.delete(id);
+    if (removedUser.affected === 0) {
+      throw new NotFoundException();
+    }
+    return { id };
   }
 }
