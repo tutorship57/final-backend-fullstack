ff8d0668f04f170896cb8b913e2eeef22b226d19
Edward_Bacara <tutorship57@gmail.com>
2026-04-07 02:58:19 +0700

    feat: add sanitization of input

--- src/workspace-member/dto/create-workspace-member.dto.ts
+++ src/workspace-member/dto/create-workspace-member.dto.ts
@@ -1,14 +1,18 @@
 import { IsArray, IsNotEmpty, IsUUID } from 'class-validator';
+import { SanitizeHtml } from 'src/common/decorators/sanitize-html.decorator';
 export class CreateWorkspaceMemberDto {
   @IsUUID()
   @IsNotEmpty()
+  @SanitizeHtml()
   user_id: string;
 
   @IsUUID()
   @IsNotEmpty()
+  @SanitizeHtml()
   workspace_id: string;
 
   @IsArray()
+  @SanitizeHtml()
   @IsUUID('all', { each: true })
   role_ids: string[];
 }
